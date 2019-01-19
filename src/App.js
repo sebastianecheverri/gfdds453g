2ca349877ce3a23381b69413f23c0cbfd1eb09d0
Sebastian Echeverri <sebastianecheverri900@gmail.com>
2019-01-18 21:30:29 -0500

    solved

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ export class App extends Component {
     return (
       <div>
         <nav>
-          {/*Aca deben ir los links de navegacion*/}
+          <Link to="page1">Page1</Link>
+          <Link to="page2">Page2</Link>
         </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+          {this.props.children}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import './index.css';
 // Implementa las rutas aca
 
 ReactDOM.render(
+  <App >
+  <Router history={hashHistory}>
+    <Route path="/" component={Page1} />
+    <Route path="Page1" component={Page1} />
+    <Route path="Page2" component={Page2} />
+    <Route path="*" component={NotFound} />
+  </Router>
+
   <App />, // Es posible que tengas que hacer un cambio en esta linea.
   document.getElementById('root')
 );
